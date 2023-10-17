714e45bec5c9ed93113ac655a86d847762a5a79c
Leozeroto <90214145+Leozeroto@users.noreply.github.com>
2023-10-17 07:39:22 +0200

    test pour css sur props

--- cantinphotographreact/src/pages/Tarifs.js
+++ cantinphotographreact/src/pages/Tarifs.js
@@ -6,16 +6,15 @@ function Tarifs() {
         <div className='tarifs'>
             <h1>Tarifs et Prestations</h1>
 
-            < Prestations
-                h2={'"Juste pour moi" 130 euros'}
-                p={'Séance pour une personne, en extérieur ou en studio'}
-            />
+                < Prestations
+                    h2={'"Juste pour moi" 130 euros'}
+                    p={'Séance pour une personne, en extérieur ou en studio'}
+                />
 
-            < Prestations
-                h2={'"Pour deux" 195 euros'}
-                p={'Pour deux personnes, en extérieur ou en studio'}
-            />
-
+                < Prestations
+                    h2={'"Pour deux" 195 euros'}
+                    p={'Pour deux personnes, en extérieur ou en studio'}
+                />
         </div>
     )
 }
