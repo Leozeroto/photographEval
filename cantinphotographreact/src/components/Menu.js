14b850c760e1b3baa9d62e74c533ea204a5466a7
Leozeroto <90214145+Leozeroto@users.noreply.github.com>
2023-10-28 22:22:23 +0200

    ajout des NavLink du menu

--- cantinphotographreact/src/components/Menu.js
+++ cantinphotographreact/src/components/Menu.js
@@ -1,12 +1,14 @@
+import { NavLink } from 'react-router-dom';
+
 import React from 'react'
 
 const Menu = () => {
   return (
     <div className='menu'>
         <ul>
-            <li>Accueil</li>
-            <li>Galerie</li>
-            <li>Tarif et prestations</li>
+            <li><NavLink to="/">Accueil</NavLink></li>
+            <li><NavLink to="/galerie">Galerie</NavLink></li>
+            <li><NavLink to="/tarifs">Tarif et prestations</NavLink></li>
             <li> Contact</li>
 
         </ul>
--- cantinphotographreact/src/pages/Galerie.js
+++ cantinphotographreact/src/pages/Galerie.js
@@ -8,16 +8,18 @@ function Galerie() {
         <div className='galerie'>
             <h1>Galerie</h1>
 
-            <select name='categorie' id='categorie'>
-                <option value="">Choisir une catégorie</option>
-                <option value="mariage">Mariage</option>
-                <option value="grossesse">Grossesse</option>
-                <option value="bebe">Bébé</option>
-                <option value="famille">Famille</option>
-                <option value="bapteme">Baptême</option>
-                <option value="couple">Couple</option>
-                <option value="portrait">Portrait</option>
-            </select>
+            <div className='select'>
+                <select name='categorie' id='categorie'>
+                    <option value="">Choisir une catégorie</option>
+                    <option value="mariage">Mariage</option>
+                    <option value="grossesse">Grossesse</option>
+                    <option value="bebe">Bébé</option>
+                    <option value="famille">Famille</option>
+                    <option value="bapteme">Baptême</option>
+                    <option value="couple">Couple</option>
+                    <option value="portrait">Portrait</option>
+                </select>
+            </div>
 
             <div>
                 <Card1 />
