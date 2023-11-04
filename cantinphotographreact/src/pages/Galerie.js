54595a1bbe029e3fbfe36119e63e089aa2a93846
Leozeroto <90214145+Leozeroto@users.noreply.github.com>
2023-11-04 19:30:00 +0100

    travail sur la page galerie

--- cantinphotographreact/src/pages/Galerie.js
+++ cantinphotographreact/src/pages/Galerie.js
@@ -5,30 +5,40 @@ import Card from '../components/Card'
 function Galerie() {
     return (
         <>
-            <div className='imgaccueil'></div>
+            <h1 className='titreGalerie'>Voici l'ensemble de ma galerie</h1>
 
-            <div className='galerie'>
-                <h1>Galerie</h1>
-
-                <div className='select'>
-                    <select name='categorie' id='categorie'>
-                        <option value="">Choisir une catégorie</option>
-                        <option value="mariage">Mariage</option>
-                        <option value="grossesse">Grossesse</option>
-                        <option value="bebe">Bébé</option>
-                        <option value="famille">Famille</option>
-                        <option value="bapteme">Baptême</option>
-                        <option value="couple">Couple</option>
-                        <option value="portrait">Portrait</option>
-                    </select>
-                </div>
-
-
-                <div className="carte">
-                    <Card />
-                </div>
-
+            <div className='select'>
+                <select name='categorie' id='categorie'>
+                    <option value="">Choisir une catégorie</option>
+                    <option value="mariage">Mariage</option>
+                    <option value="grossesse">Grossesse</option>
+                    <option value="bebe">Bébé</option>
+                    <option value="famille">Famille</option>
+                    <option value="bapteme">Baptême</option>
+                    <option value="couple">Couple</option>
+                    <option value="portrait">Portrait</option>
+                </select>
             </div>
+
+
+            <div className="carte">
+                <Card
+                    img='https://www.zooplus.fr/magazine/wp-content/uploads/2019/06/comprendre-le-langage-des-chats.jpg'
+                />
+
+                <Card
+                    img='https://www.zooplus.fr/magazine/wp-content/uploads/2019/06/comprendre-le-langage-des-chats.jpg'
+                />
+
+                <Card
+                    img='https://www.zooplus.fr/magazine/wp-content/uploads/2019/06/comprendre-le-langage-des-chats.jpg'
+                />
+
+                <Card
+                    img='https://www.zooplus.fr/magazine/wp-content/uploads/2019/06/comprendre-le-langage-des-chats.jpg'
+                />
+            </div>
+
 
 
         </>
