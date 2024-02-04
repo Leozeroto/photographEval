b524450629979eecc5594b8894145523d389b821
Leozeroto <90214145+Leozeroto@users.noreply.github.com>
2024-02-04 20:06:08 +0100

    travail css page galerie

--- cantinphotographreact/src/pages/Galerie.js
+++ cantinphotographreact/src/pages/Galerie.js
@@ -1,8 +1,12 @@
+import { useState } from 'react'
 import React from 'react'
 import Card from '../components/Card'
 
 
 function Galerie() {
+
+const [filtreGalerie, setFiltreGalerie] = useState ([]);
+
     return (
         <>
             <h1 className='titreGalerie'>Voici ma galerie photo</h1>
@@ -27,33 +31,39 @@ function Galerie() {
                     p={"Photos de baptême"}
                 />
                 <Card
+                    img={"/img/famille/family-2485714_640.jpg"}
                     h4={"Famille"}
                     p={"Photos de famille"}
                 />
 
                 <Card
+                    img={"/img/grossesse/pregnant-775036_640.jpg"}
                     h4={"Grossesse"}
                     p={"Photos de grossesse"}
                 />
 
                 <Card
+                    img={"/img/portrait/child-817373_640.jpg"}
                     h4={"Portrait"}
                     p={"Photos de portrait"}
                 />
 
                 <Card
+                    img={"/img/bebe/bebe1.jpg"}
                     h4={"Bébé"}
                     p={"Photos de bébé"}
                 />
 
 
                 <Card
+                    img={"/img/couple/couple-437968_640.jpg"}
                     h4={"Couple"}
                     p={"Photos de couple"}
                 />
 
 
                 <Card
+                    img={"/img/mariage/bride-1486004_640.jpg"}
                     h4={"Mariage"}
                     p={"Photos de mariage"}
                 />
